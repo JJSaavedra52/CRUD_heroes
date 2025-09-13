13b50f8d2b2826781239e3d0246ca73d28bc9de7
Juan David Garzon Vallejo <juanchope039@gmail.com>
2025-09-13 18:34:45 -0500

    actualización de composición de las screens

--- lib/screens/episode_screen.dart
+++ lib/screens/episode_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_componentes/providers/providers.dart';
 import 'package:flutter/material.dart';
 
 class EpisodesScreen extends StatefulWidget {
@@ -8,12 +9,42 @@ class EpisodesScreen extends StatefulWidget {
 }
 
 class _EpisodesScreenState extends State<EpisodesScreen> {
+  List<dynamic> locations = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchLocations();
+  }
+
+  Future<void> fetchLocations() async {
+    final data = await RickMortyProvider.getOnDisplayLocations();
+    setState(() {
+      locations = data.take(10).toList();
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Rick & Morty Episodes')),
-        body: Text("data"),
+      appBar: AppBar(title: const Text('Rick & Morty Episodes')),
+      body: /*isLoading
+          ? */const Center(child: CircularProgressIndicator())/*
+          : CustomListView(
+        fuente: RickMortyProvider.getOnDisplayLocations(),
+        image: '',
+        name: 'name',
+        descriptions: [
+          'id',
+          'type',
+          'dimension',
+          'url',
+          'created',
+        ],
+        extras: 'residents',
+      ),*/
     );
   }
 }
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -11,30 +11,17 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-  List<dynamic> locations = [];
-  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    fetchLocations();
-  }
-
-  Future<void> fetchLocations() async {
-    final data = await RickMortyProvider.getOnDisplayLocations();
-    setState(() {
-      locations = data.take(10).toList();
-      isLoading = false;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Rick & Morty Episodes')),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : CustomListView(
+      body: CustomListView(
         fuente: RickMortyProvider.getOnDisplayLocations(),
         image: '',
         name: 'name',
@@ -50,42 +37,3 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
-/**
- * {
-    "": 1,
-    "": "Earth (C-137)",
-    "": "Planet",
-    "": "Dimension C-137",
-    "": [
-    "https://rickandmortyapi.com/api/character/38",
-    "https://rickandmortyapi.com/api/character/45",
-    "https://rickandmortyapi.com/api/character/71",
-    "https://rickandmortyapi.com/api/character/82",
-    "https://rickandmortyapi.com/api/character/83",
-    "https://rickandmortyapi.com/api/character/92",
-    "https://rickandmortyapi.com/api/character/112",
-    "https://rickandmortyapi.com/api/character/114",
-    "https://rickandmortyapi.com/api/character/116",
-    "https://rickandmortyapi.com/api/character/117",
-    "https://rickandmortyapi.com/api/character/120",
-    "https://rickandmortyapi.com/api/character/127",
-    "https://rickandmortyapi.com/api/character/155",
-    "https://rickandmortyapi.com/api/character/169",
-    "https://rickandmortyapi.com/api/character/175",
-    "https://rickandmortyapi.com/api/character/179",
-    "https://rickandmortyapi.com/api/character/186",
-    "https://rickandmortyapi.com/api/character/201",
-    "https://rickandmortyapi.com/api/character/216",
-    "https://rickandmortyapi.com/api/character/239",
-    "https://rickandmortyapi.com/api/character/271",
-    "https://rickandmortyapi.com/api/character/302",
-    "https://rickandmortyapi.com/api/character/303",
-    "https://rickandmortyapi.com/api/character/338",
-    "https://rickandmortyapi.com/api/character/343",
-    "https://rickandmortyapi.com/api/character/356",
-    "https://rickandmortyapi.com/api/character/394"
-    ],
-    "": "https://rickandmortyapi.com/api/location/1",
-    "": "2017-11-10T12:42:04.162Z"
-    },
- */
