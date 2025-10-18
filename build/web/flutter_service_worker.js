5c29a49e10d6d06c1d8e00b1a14bca847a941b57
Juan José Saavedra Realpe <juanjsaavedra52@gmail.com>
2025-10-18 11:55:09 -0500

    Rebuild web after auth_service fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "15c598ba97357678c8e689a3157c1246
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "240eed6c05c71c679c9bab41c870b044",
+"flutter_bootstrap.js": "d0836a5492143ec4142da719ceebc3a8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "5108b6d412f720f6a6a51b8bf36d2b48",
 "/": "5108b6d412f720f6a6a51b8bf36d2b48",
-"main.dart.js": "7f41b37569c5f1d8961b4d4786a97e45",
+"main.dart.js": "2cbe6048f31a86d01010022ebccd50ae",
 "manifest.json": "cb887d029f1968479c6a491c6ee621a9",
 "version.json": "06d4ad405356d92042124c63f2f8460b"};
 // The application shell files that are downloaded before a service worker can
