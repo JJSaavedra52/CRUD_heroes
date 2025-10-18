09a21b0c64bec8973353cffc04d5f60e560aad31
Juan David Garzon Vallejo <juanchope039@gmail.com>
2025-10-18 16:06:00 -0500

    se genera release para deploy2
    Signed-off-by: Juan David Garzon Vallejo <juanchope039@gmail.com>

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "530338814"
+    serviceWorkerVersion: "4176593879"
   }
 });
